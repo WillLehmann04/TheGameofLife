a3f02af66c7ce36a582bbcda3c4747324eae3776
Will Lehmann <will.lehmann4@gmail.com>
2023-04-21 19:38:10 +0930

    Update getClickPosition.m
    Finalised the code and comments for this section. Nice!

--- getClickPosition.m
+++ getClickPosition.m
@@ -1,7 +1,7 @@
 function [roundedx, roundedy] = getClickPosition()
     % Function to get the users click position and determine its
     % coordinates.
-    [col, row, key] = ginput(1); % Retrieves key, x and y coords of user input.
-    if (key~=1) roundedx=0;roundedy=0;return; end % If the key is anything but left click, stop running.
-    roundedx = round(col); roundedy = round(row); % Output the final x and y coordinates of user click.
+    [col, row, key] = ginput(1);                    % Retrieves key, x and y coords of user input.
+    if (key~=1) roundedx=0;roundedy=0;return; end   % If the key is anything but left click, stop running.
+    roundedx = round(col); roundedy = round(row);   % Output the final x and y coordinates of user click.
 end
