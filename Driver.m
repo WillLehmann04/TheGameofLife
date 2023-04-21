7b98036764b96fede242a132ad6cc6e47fec7f15
Will Lehmann <will.lehmann4@gmail.com>
2023-04-21 19:19:34 +0930

    Driver.m Comment update. 1/2
    Added some comments and did some general clean up.

--- Driver.m
+++ Driver.m
@@ -1,51 +1,63 @@
+% General clean up of the workspace.
 clear;
 clc;
 
-alive = [0,0];
-board = zeros(10);
+% General start params.
+boardSize = input("Please enter a board size: ");
+while ((boardSize <= 0) || (rem(boardSize,1) ~= 0)) boardSize = input("Please enter a board size: "); end % Makes sure the input is above 0 and a integer.
+generationSpeed = input("Please input a generation speed (s): ");
+while ((generationSpeed <= 0)) generationSpeed = input("Please input a generation speed (s): "); end % Makes sure the input is a number above 0.
 
-figure();
-imagesc(board);
+% Global variables commonly needed.
+alive = [0,0]; % [0,0] needed to be able to compare the size of matrix later to remove items.
+board = zeros(boardSize); % Creates board of 0's for size boardSize x boardSize.
+figure(); % Initiates the figure used to display the board of zeros.
+imagesc(board); % Displays the board on the previously generated figure.
 
-finishedSetup = false;
+
+finishedSetup = false; % Purely for the loop, to determine when it should end.
 while ~finishedSetup
-    [row, col] = getClickPosition();
-    fprintf("Col: %d\nRow: %d\n\n", col, row)
-    if (col == 0 || row == 0)
-        finishedSetup = true;
+    % Loop that gets the setup parameters.
+
+    [row, col] = getClickPosition(); % Gets the result of the function getClickPosition for later use in the loop.
+    %fprintf("Col: %d\nRow: %d\n\n", col, row)
+    if (col == 0 || row == 0) % Because the output of getClickPosition is 0 if a key other than left click is pressed, this exits the loop.
+        finishedSetup = true; % Makes the loop stop, setting the loop condition to true;
     end
-    if (ismember([col, row], alive, 'rows'))
-        for i = 1:size(alive, 1)
-            if all(alive(i,:) == [col, row])
-                idx = i;
-                break;
+    if (ismember([col, row], alive, 'rows')) % Checks to see if the col and row are a part of the alive matrix
+        for i = 1:size(alive, 1) % if it is apart of the alive matrix it loops through all alive 
+            if all(alive(i,:) == [col, row]) % Search through all alive and find the index of the current row and col
+                idx = i; % assign the index, to var idx
+                break; % breaks out of the loop as its unnecessary to keep looping if the index has been found.
             end
         end
-        alive(idx,:) = [];
+        alive(idx,:) = []; % remove the row and col from alive matrix.
     else
-        alive = [alive; col, row];
+        alive = [alive; col, row]; % add the row and col to the alive matrix.
     end
-    displayBoard(board, alive);
+    displayBoard(board, alive); % display the board to the current figure.
 end
 
 generation = 0;
-while true
+squareCounter=size(alive,1);
+while ~isempty(alive)
     nextGeneration = [];
     for col = 2:size(board, 1)+1
         for row = 2:size(board, 2)+1
             if (determineStatus(col, row, alive, board))
                 nextGeneration = [nextGeneration; col, row];
+                squareCounter = squareCounter + 1;
             end
         end
     end
 
-
     alive = nextGeneration;
     displayBoard(board, alive);
 
-
     generation = generation + 1;
     fprintf("Generation: %d", generation);
-    pause(1)
+    pause(generationSpeed);
 end
-
+clc;
+close;
+fprintf("Finished on generation: %d\nTotal number of squares produced: %d", generation, squareCounter);
