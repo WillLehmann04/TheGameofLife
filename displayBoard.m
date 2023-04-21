698908244813d18eddb5d456142e4e96cc1a6259
Will Lehmann <will.lehmann4@gmail.com>
2023-04-21 19:42:29 +0930

    Version 1.0.6
    Updated the displayBoard.m mostly, added some comments here and there but mainly cleanup.

--- displayBoard.m
+++ displayBoard.m
@@ -1,12 +1,14 @@
 function [] = displayBoard(board, alive)
-    for col = 1:size(board,1)
-        for row = 1:size(board,2)
-            for l = 1:size(alive, 1)
-                if (alive(l, 1) == col && alive (l, 2) == row)
-                    board(col, row) = 2;
+    % Function to display all the alive matrix on the board and into the
+    % figure.
+    for col = 1:size(board,1)                                   % Loop through every col 
+        for row = 1:size(board,2)                               % Loop through every row 
+            for l = 1:size(alive, 1)                            % Loop through every item in alive.
+                if (alive(l, 1) == col && alive (l, 2) == row)  % If the col and row match something in alive move inside if statment
+                    board(col, row) = 2;                        % change the value of it to 2.
                 end
             end
         end
     end
-    imagesc(board);
+    imagesc(board);     % Override the current figure with new details.
 end
