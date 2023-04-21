a4c43bccc4ac3f90be431fd2f26fba6c497bad98
Will Lehmann <will.lehmann4@gmail.com>
2023-04-21 18:05:25 +0930

    Update determineStatus.m
    Have the output scrolling in the console to determine if the 8 squares around the centre square are alive or not to influence its decisions,

--- determineStatus.m
+++ determineStatus.m
@@ -5,10 +5,19 @@ function [ nextGeneration ] = determineStatus(col, row, alive, board)
     for squares = 1:size(alive,1)
         tempBoard(alive(squares, 1)+1, alive(squares, 2)+1) = 2;
     end
-    
     clc;
     tempBoard(col, row) = 3;
+    
+    for i = -1:1
+        for p = -1:1
+            if (i == 0 && p == 0) continue; end
+            tempBoard(col+i, row+p) = 4;
+        end
+    end
+
+
+
     nextGeneration = false;
     disp(tempBoard)
-    pause(100)
+    pause(0.3)
 end
