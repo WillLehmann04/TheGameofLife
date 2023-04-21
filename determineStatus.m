46c6b84e37cbdefa512ac3e2129aeb4f03bad47e
Will Lehmann <will.lehmann4@gmail.com>
2023-04-21 20:22:42 +0930

    Update. 1.0.8
    Added comments, general optimisation and bug fixes.

--- determineStatus.m
+++ determineStatus.m
@@ -1,31 +1,41 @@
 function [ nextGeneration ] = determineStatus(col, row, alive, board)
-    tempBoard = ones(size(board)+2);
-    tempBoard(2:end-1,2:end-1) = board;
-
-    for squares = 1:size(alive,1)
-        tempBoard(alive(squares, 1)+1, alive(squares, 2)+1) = 2;
-    end
-    clc;
-    tempBoard(col, row) = 3;
+    % determineStatus determines whether the col, row (coordinates) should
+    % be alive or dead.
+    
+    % General variables.
     totalCounter = 0;
-    for i = -1:1
-        for p = -1:1
-            if (i == 0 && p == 0) continue; end
-            %tempBoard(col+i, row+p) = 4;
-            for n = 1:size(alive, 1)
-                if (alive(n,1) == col+i && alive(n, 2) == row+p) totalCounter=totalCounter+1; end
+    tempBoard = ones(size(board)+2);    % Creates a temp board 2 bigger than the size of the actual game board.
+    tempBoard(2:end-1,2:end-1) = board; % Sets the gameboard inside the tempBoard.
+                                        
+                                        % Example if above:
+                                        % 1 1 1 1 1 1 1 1 1
+                                        % 1 0 0 0 0 0 0 0 1
+                                        % 1 0 0 0 0 0 0 0 1
+                                        % 1 0 0 0 0 0 0 0 1
+                                        % 1 0 0 0 0 0 0 0 1
+                                        % 1 1 1 1 1 1 1 1 1
+
+
+    for i = -1:1            % For one behind, the current col, and one front square.
+        for p = -1:1        % For one behind, the current col, and one front square.
+
+                            % These two foreloops create a loop that looks like:
+                            % 1 1 1
+                            % 1 0 1
+                            % 1 1 1
+
+            if (i == 0 && p == 0) continue; end % Ensures it skips its self thus not adding 1 to the total counter if it's a live cell.
+            for n = 1:size(alive, 1)                                                                % Loops through every curerntly alive cells.
+                if (alive(n,1) == col+i && alive(n, 2) == row+p) totalCounter=totalCounter+1; end   % for every live square in the surrounding squares add one to totalCounter.
             end
         end
     end
     
-    isAlive = false;
-    for g = 1:size(alive, 1)
-        if (alive(g, 1) == col && alive(g,2) == row) isAlive = true; end
-    end
+    if ismember([col, row], alive, 'rows') isAlive = true; else isAlive = false; end % Determines whether the current row and col is previously alive.
 
-    if ((isAlive && ~(totalCounter <= 1 || totalCounter >= 4)) || (~isAlive && totalCounter == 3))
-        nextGeneration = true;
+    if ((isAlive && ~(totalCounter <= 1 || totalCounter >= 4)) || (~isAlive && totalCounter == 3)) % Rules for determining whether the cell is alive or dead.
+        nextGeneration = true;      % Cell is alive
         return
     end
-    nextGeneration = false;
+    nextGeneration = false;         % Cell is dead.
 end
